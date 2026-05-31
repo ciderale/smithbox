9f6ccde9c4adc64d6f58ebdd15e2c50427647d27
Alain Lehmann <alain.lehmann@gmail.com>
2026-05-31 03:53:05 +0200

    add nix-docker.nix

---
+++ nix/nix-docker.nix
@@ -0,0 +1,53 @@
+{
+  pkgs ? import <nixpkgs> {},
+  #user ? ''"$USER"'',
+  user ? "claude",
+  docker-args ? ''-v "$PWD:/project" -w /project'',
+}: let
+  dockerfile = pkgs.writeText "Dockerfile" ''
+    FROM debian:bookworm-slim
+
+    ARG USER_NAME=claude
+
+    RUN apt-get update \
+      && apt-get install -y curl \
+      && rm -rf /var/lib/apt/lists/* \
+      && useradd -m $USER_NAME \
+      && curl -sSfL https://artifacts.nixos.org/nix-installer | sh -s -- install linux --init none --enable-flakes --no-confirm \
+      && chown -R $USER_NAME:$USER_NAME /nix
+
+    USER $USER_NAME
+    RUN mkdir ~/.cache && mkdir /nix/_cache_nix && ln -s /nix/_cache_nix ~/.cache/nix
+    ENV PATH="/home/''${USER_NAME}/.nix-profile/bin:/nix/var/nix/profiles/default/bin:''${PATH}"
+  '';
+  docker-nix = pkgs.writeShellApplication {
+    name = "docker-nix";
+    text = ''
+      USER_NAME=${user}
+      VOL="docker-nix-store-$USER_NAME"
+
+      IMAGE_NAME="docker-nix-$USER_NAME"
+      TAG="${builtins.hashFile "md5" dockerfile}"
+      IMAGE="$IMAGE_NAME:$TAG"
+
+      function init() {
+        if ! docker image inspect "$IMAGE" >/dev/null 2>&1; then
+          docker build -t "$IMAGE_NAME:latest" -t "$IMAGE" --build-arg "USER_NAME=$USER_NAME" - < ${dockerfile}
+          docker volume rm "$VOL" || true
+        fi
+        docker volume create "$VOL" > /dev/null
+      }
+
+      function run() {
+        docker run --rm -ti -v "$VOL:/nix" ${docker-args} "$IMAGE" "$@"
+      }
+
+      function testrun() {
+        run nix run 'nixpkgs#hello'
+      }
+
+      init && run "$@"
+    '';
+  };
+in
+  docker-nix
