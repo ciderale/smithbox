53ce76d8408c7c898295a7c5045cee255802a378
Alain Lehmann <alain.lehmann@gmail.com>
2026-06-13 17:44:50 +0200

    make docker-nix-shell script more minimal
    - extract unessential concerns (projectFolder, stripProject, gitRoot)
    - add exteraPreamble & extraDockerfile options
    - by default, container runs as local user & home dir
    - by default, container mounts and works in PWD

--- nix/docker-nix-shell.module.nix
+++ nix/docker-nix-shell.module.nix
@@ -7,32 +7,26 @@
   inherit (lib) types;
 
   cfg = config.docker-sandbox;
-  mountFormat = mode: path: ''-v "${path}":"$(stripProject "${path}")":${mode} '';
+  mountFormat = mode: path: ''-v "${path}":"${path}":${mode} '';
   roMounts = lib.concatMapStrings (mountFormat "ro") cfg.mounts.readOnly;
   rwMounts = lib.concatMapStrings (mountFormat "rw") cfg.mounts.readWrite;
   docker-run-options = [
-    "-w ${cfg.projectDir}"
+    ''-w "$PWD"''
     roMounts
     rwMounts
   ];
   enableDirenv = cfg.direnv.enable;
-  cmd = ''"''${@:-bash}"'';
   docker-container-args =
     if enableDirenv
-    then ''sh -c "direnv allow && direnv exec . \"''${@:-bash}\" " ''
-    else ''nix develop . --command ${cmd}'';
+    then ''sh -c "sleep 1 && direnv allow && direnv exec . \"''${@:-bash}\" " ''
+    else ''sh -c "sleep 1 && nix develop --quiet . --command \"''${@:-bash}\" "'';
+  extraDockerfile = lib.optionals enableDirenv [
+    "RUN nix profile add nixpkgs#direnv nixpkgs#nix-direnv nixpkgs#bashInteractive"
+  ];
 in {
   options.docker-sandbox = {
     enable = lib.mkEnableOption "docker-sandbox";
     direnv.enable = lib.mkEnableOption "direnv" // {default = true;};
-    user = lib.mkOption {
-      type = types.str;
-      default = "claude";
-    };
-    projectDir = lib.mkOption {
-      type = types.str;
-      default = "/project";
-    };
     mounts = {
       readOnly = lib.mkOption {
         type = types.listOf types.str;
@@ -47,9 +41,7 @@ in {
   config = lib.mkIf cfg.enable {
     packages = [
       (import ./docker-nix-shell.nix {
-        inherit pkgs docker-run-options docker-container-args enableDirenv;
-        inherit (cfg) user projectDir;
-        gitRoot = config.git.root.shellVariable;
+        inherit pkgs extraDockerfile docker-run-options docker-container-args;
       })
     ];
   };
--- nix/docker-nix-shell.nix
+++ nix/docker-nix-shell.nix
@@ -1,48 +1,42 @@
 {
   pkgs ? import <nixpkgs> {},
-  #user ? ''"$USER"'',
-  user ? "claude",
-  projectDir ? "/project",
-  gitRoot,
-  docker-run-options ? [''-v "${gitRoot}:${projectDir}" -w ${projectDir}''],
+  userName ? "$USER",
+  homeDir ? "$HOME",
+  extraPreamble ? "",
+  extraDockerfile ? [],
+  # default to work in the current working directory in the container
+  docker-run-options ? ["-w" ''"$PWD"'' ''-v "$PWD:$PWD"''],
   docker-container-args ? ''"''${@}"'',
-  enableDirenv ? false,
 }: let
   inherit (pkgs) lib;
 
   dockerfile = pkgs.writeText "Dockerfile" ''
     FROM debian:bookworm-slim
 
-    ARG USER_NAME=claude
+    ARG USER_NAME
+    ARG HOME
 
     RUN apt-get update \
       && apt-get install -y curl \
       && rm -rf /var/lib/apt/lists/* \
-      && useradd -m $USER_NAME \
-      && curl -sSfL https://artifacts.nixos.org/nix-installer | sh -s -- install linux --init none --enable-flakes --no-confirm \
+      && useradd -m -d $HOME $USER_NAME \
+      && curl -sSfL https://artifacts.nixos.org/nix-installer \
+            | sh -s -- install linux --init none --enable-flakes --no-confirm \
       && chown -R $USER_NAME:$USER_NAME /nix
 
     USER $USER_NAME
-    RUN mkdir /nix/_cache_nix && ln -s /nix/_cache_nix ~/.cache
-    ENV PATH="/home/''${USER_NAME}/.nix-profile/bin:/nix/var/nix/profiles/default/bin:''${PATH}"
+    ENV HOME=$HOME
+    RUN mkdir /nix/_cache_nix && ln -s /nix/_cache_nix "$HOME/.cache"
+    ENV PATH="''${HOME}/.nix-profile/bin:/nix/var/nix/profiles/default/bin:''${PATH}"
 
-    ${
-      if enableDirenv
-      then "RUN nix profile install nixpkgs#direnv nixpkgs#nix-direnv"
-      else ""
-    }
-
-    # workaround for /project being root owned when `docker run ... command`
-    RUN cat > ~/.gitconfig <<EOF
-    [safe]
-      directory = ${projectDir}
-    EOF
+    ${lib.concatStringsSep "\n" extraDockerfile}
   '';
   docker-nix-shell = pkgs.writeShellApplication {
     name = "docker-nix-shell";
     extraShellCheckFlags = ["-e" "SC2145"];
     text = ''
-      USER_NAME=${user}
+      USER_NAME=${userName}
+      HOME_DIR=${homeDir}
       VOL="docker-nix-store-$USER_NAME"
 
       IMAGE_NAME="docker-nix-$USER_NAME"
@@ -51,15 +45,16 @@
 
       function init() {
         if ! docker image inspect "$IMAGE" >/dev/null 2>&1; then
-          docker build -t "$IMAGE_NAME:latest" -t "$IMAGE" --build-arg "USER_NAME=$USER_NAME" - < ${dockerfile}
+          docker build -t "$IMAGE_NAME:latest" -t "$IMAGE" \
+            --build-arg "USER_NAME=$USER_NAME" \
+            --build-arg "HOME=$HOME_DIR" \
+            - < ${dockerfile}
           docker volume rm "$VOL" || true
         fi
         docker volume create "$VOL" > /dev/null
       }
 
-      function stripProject() {
-        echo "''${1/#${gitRoot}/${projectDir}}"
-      }
+      ${extraPreamble}
 
       function run() {
         docker run --rm -ti -v "$VOL:/nix" ${lib.concatStringsSep " " docker-run-options} "$IMAGE" ${docker-container-args}
