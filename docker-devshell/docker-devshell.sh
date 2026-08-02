bea8f00ecce92a31cc4496ac3071886ae2322986
Alain Lehmann <alain.lehmann@gmail.com>
2026-08-02 13:25:26 +0200

    rename run.sh => docker-devshell.sh

--- docker-devshell/docker-devshell.sh
+++ docker-devshell/docker-devshell.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
+set -Eeuo pipefail
 
-PROJECT_ROOT="$PWD/.."
-PROJECT_DIR=/data
-HOME_DIR=/home/ale
-NEW_UID=$(id -u)
-NEW_GID=$(id -g)
+# host location of the project (use git root unless specified)
+: "${PROJECT_ROOT:=$(git rev-parse --show-toplevel)}"
+
+#the method to setup the dev environment
 #MODE=plain
 #MODE=nix-develop
 MODE=direnv
 
+# various settings for the container environment
+# note: home is not mounted, but serves for persisting across sessions
+HOME_DIR=$HOME
+NEW_UID=$(id -u)
+NEW_GID=$(id -g)
+PROJECT_DIR=/data
+
+# Naming of the image
 IMAGE_NAME=docker-devshell
 IMAGE="$IMAGE_NAME:latest"
 
+# Naming for the volums
 slug() {
 	printf '%s' "$1" | tr -c '[:alnum:]._-' '-'
 }
 HOME_VOLUME=docker-nix-$(slug "$HOME_DIR")
 STORE_VOLUME=docker-nix-store
 
-
+# actual execution
 docker build --quiet -t "$IMAGE" .
 
 docker run --rm -ti \
@@ -27,4 +36,5 @@ docker run --rm -ti \
 	-v $HOME_VOLUME:$HOME_DIR -e HOME=$HOME_DIR\
 	-e MODE=$MODE \
 	-v "$PROJECT_ROOT:$PROJECT_DIR" -w "$PROJECT_DIR" \
+	"${DOCKER_RUN_ARGS[@]}" \
 	"$IMAGE" "$@"
